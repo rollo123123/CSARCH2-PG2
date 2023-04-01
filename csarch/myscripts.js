9b3f6009f33725a0a3f02f37a1fda8a273c886b4
foodequalslife2 <114330898+foodequalslife2@users.noreply.github.com>
2023-04-02 04:46:18 +0800

    Add files via upload

---
+++ csarch/myscripts.js
@@ -0,0 +1,9 @@
+
+
+function showDecimals() {
+    document.getElementById("decimal-input").style.display = "block";
+  }
+  
+  function hideDecimals() {
+    document.getElementById("decimal-input").style.display = "none";
+  }
