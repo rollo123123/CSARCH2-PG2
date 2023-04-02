841f90ac7d5532e1a2c06ef818bd112cbf801472
foodequalslife2 <114330898+foodequalslife2@users.noreply.github.com>
2023-04-02 11:27:29 +0800

    Delete csarch directory

--- csarch/myscripts.js
+++
@@ -1,9 +0,0 @@
-
-
-function showDecimals() {
-    document.getElementById("decimal-input").style.display = "block";
-  }
-  
-  function hideDecimals() {
-    document.getElementById("decimal-input").style.display = "none";
-  }
